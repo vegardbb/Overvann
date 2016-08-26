22980f6e09ae7e6ea63d9831b1aed2cc84c359d9
Peder Voldnes Langdal <peder.langdal@gmail.com>
2016-08-26 10:14:33 +0200

    Simplified frontpage and deploy-script

--- deploy_code.sh
+++ deploy_code.sh
@@ -2,47 +2,41 @@
 
 # Script used to deploy the wiki part or the node part of ovase.no
 #
-# Usage: 	./deploy_code.sh ovase 			to deploy the node part
+# Usage: 	./deploy_code.sh ovase 			to deploy the main ovase page
 # 			./deploy_code.sh wiki 			to deploy the wiki part
 # 			./deploy_code.sh ovase wiki 	to deploy both parts
 #
 
-OVASE_DIR=~/ovase_web
+OVASE_DIR=~/var/www/html
 WIKI_DIR=/var/www/html/wiki
 BACKUP_ROOT=~/deploy_backups
 BACKUP_DIR="${BACKUP_ROOT}/backup_$(date +%Y%m%d_%H%M%S)"
 
-DEP_WIKI=false
-DEP_OVASE=false
+DEPLOY_WIKI=false
+DEPLOY_OVASE=false
 
 if [ "$1" = "wiki" ]; then
-	DEP_WIKI=true
+	DEPLOY_WIKI=true
 fi
 
 if [ "$1" = "ovase" ]; then
-	DEP_OVASE=true
+	DEPLOY_OVASE=true
 fi
 
 if [ "$2" = "wiki" ]; then
-	DEP_WIKI=true
+	DEPLOY_WIKI=true
 fi
 
 if [ "$2" = "ovase" ]; then
-	DEP_OVASE=true
+	DEPLOY_OVASE=true
 fi
 
-echo "- Deploy ovase? $DEP_OVASE , Deploy wiki? $DEP_WIKI -"
+echo "- Deploy ovase? $DEPLOY_OVASE , Deploy wiki? $DEPLOY_WIKI -"
 
 
 # Stop apache and the node-service
-echo "========== Stopping web servers =========="
-if [ "$DEP_WIKI" = true ]; then
-	sudo service httpd stop
-fi
-
-if [ "$DEP_OVASE" = true ]; then
-	sudo service ovase_yp stop
-fi
+echo "========== Stopping web server =========="
+sudo service httpd stop
 
 # Sets extended pattern matching options in the bash shell
 shopt -s extglob
@@ -50,25 +44,29 @@ shopt -s extglob
 # Do backups
 echo "========== Creating backups into '~/deploy_backups' =========="
 mkdir $BACKUP_DIR
-if [ "$DEP_WIKI" = true ]; then
+if [ "$DEPLOY_WIKI" = true ]; then
 	cp -rf $WIKI_DIR $BACKUP_DIR
 fi
 
-if [ "$DEP_OVASE" = true ]; then
-	cp -rf $OVASE_DIR $BACKUP_DIR
+if [ "$DEPLOY_OVASE" = true ]; then
+	pushd .
+	cd $OVASE_DIR
+	cp -rf !(wiki) $BACKUP_DIR
+	popd
 fi
 
 # Copy files to ovase_web (the portion of the site which is not the wiki)
-echo "========== Copying ovase files =========="
-if [ "$DEP_OVASE" = true ]; then
+if [ "$DEPLOY_OVASE" = true ]; then
+	echo "========== Copying ovase files =========="
 	cp -rf !(wiki|configuration|deploy_code.sh|install_app_service.sh) $OVASE_DIR
 fi
 
 # Copy files to /var/www/html (the wiki-portion of the site)
-echo "========== Copying wiki files =========="
-if [ "$DEP_WIKI" = true ]; then
+if [ "$DEPLOY_WIKI" = true ]; then
+	echo "========== Copying wiki files =========="
 	cd wiki
 	cp -rf !(images|cache) $WIKI_DIR
+	cd ..
 fi
 
 # Disable extended pattern matching
@@ -76,12 +74,6 @@ shopt -u extglob
 
 # Start apache and the node-service again
 echo "========== Restarting web servers =========="
-if [ "$DEP_WIKI" = true ]; then
-	sudo service httpd start
-fi
-
-if [ "$DEP_OVASE" = true ]; then
-	sudo service ovase_yp start
-fi
+sudo service httpd start
 
 echo "========== Deployment (attempt) completed! =========="
