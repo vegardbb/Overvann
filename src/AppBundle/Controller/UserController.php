0dd67a2796ab3ad49f2993372caa6eeb20877b42
vegardbb <vegardbb@stud.ntnu.no>
2016-09-19 15:06:29 +0200

    User controller peerforms now a permament redirect to login page when registration is done.

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -13,74 +13,74 @@ use Symfony\Component\HttpFoundation\Request;
 class UserController extends Controller
 {
 
-    public function registerAction(Request $request)
-    {
-        // 1) build the form
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
+	public function registerAction(Request $request)
+	{
+		// 1) build the form
+		$user = new User();
+		$form = $this->createForm(UserType::class, $user);
 
-        // 2) handle the submit (will only happen on POST)
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+		// 2) handle the submit (will only happen on POST)
+		$form->handleRequest($request);
+		if ($form->isSubmitted() && $form->isValid()) {
 
-            // 3) Encode the password and set user salt (you could also do this via Doctrine listener)
+			// 3) Encode the password and set user salt (you could also do this via Doctrine listener)
 
-            // Generate random salt. Warning: The algorithm may fail. TODO: Investigate bcrypt (which Symfony recommends) and other better algorithms for salting passwords.
-            $salt=$this->generateSalt();
+			// Generate random salt. Warning: The algorithm may fail. TODO: Investigate bcrypt (which Symfony recommends) and other better algorithms for salting passwords.
+			$salt=$this->generateSalt();
 
-            // Hash password
-            $pass_hash = $this->get('security.encoder_factory')->getEncoder(User::class)->encodePassword($form->get('password')->getData(), $salt);
+			// Hash password
+			$pass_hash = $this->get('security.encoder_factory')->getEncoder(User::class)->encodePassword($form->get('password')->getData(), $salt);
 
 			$user->setPassword($pass_hash);
-            $user->setSalt($salt);
+			$user->setSalt($salt);
 
-            // Authorize User as... USER.
-            $user->addRole("ROLE_USER");
+			// Authorize User as... USER.
+			$user->addRole("ROLE_USER");
 			
 			// TODO: Notify ADMINs that a new user has registered, and that they need to be validated. An ADMIN
 			// may then send an activation email at their leisure
 			$user->setIsActive(1); // For now, you may pass...
 			
 			// 4) save the User!
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($user);
+			$em->flush();
 
-            // TODO:
+			// TODO:
 			// ... do any other work - like sending them an email, etc
-            // maybe set a "flash" success message for the user
+			// maybe set a "flash" success message for the user
 
+			// permament redirect to login.
+			return $this->redirectToRoute('login',array(),301);
+		}
 
-			return $this->redirectToRoute('login');
-        }
+		return $this->render(
+			'login/register.html.twig',
+			array('form' => $form->createView())
+		);
+	}
+	private function validPassLen($password){
+		return strlen($password)>=7;
+	}
 
-        return $this->render(
-            'login/register.html.twig',
-            array('form' => $form->createView())
-        );
-    }
-    private function validPassLen($password){
-        return strlen($password)>=7;
-    }
+	/**
+	 * Generates a Salt
+	 *
+	 * Generates a random 64-byte long string by converting a 32 byte binary
+	 * openssl pseudo random string into a 64 byte hex string.
+	 *
+	 * @return null|string  the salt. If null is returned, something went wrong.
+	 */
+	private function generateSalt()
+	{
+		$salt = null;
+		$isSecure = 0;
+		$ATTEMPTS_LIMIT = 999; // fail-safe for infinite loop
 
-    /**
-     * Generates a Salt
-     *
-     * Generates a random 64-byte long string by converting a 32 byte binary
-     * openssl pseudo random string into a 64 byte hex string.
-     *
-     * @return null|string  the salt. If null is returned, something went wrong.
-     */
-    private function generateSalt()
-    {
-        $salt = null;
-        $isSecure = 0;
-        $ATTEMPTS_LIMIT = 999; // fail-safe for infinite loop
-
-        while (!$isSecure && $ATTEMPTS_LIMIT > 0) {
-            $salt = bin2hex(openssl_random_pseudo_bytes(32, $isSecure));
-            $ATTEMPTS_LIMIT--;
-        }
-        return $salt;
-    }
+		while (!$isSecure && $ATTEMPTS_LIMIT > 0) {
+			$salt = bin2hex(openssl_random_pseudo_bytes(32, $isSecure));
+			$ATTEMPTS_LIMIT--;
+		}
+		return $salt;
+	}
 }
