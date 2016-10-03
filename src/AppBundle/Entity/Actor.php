26396726abbddd0b4bae22e5e90228ae7dabbe3d
Jonas Laksen <jonaslaksen@live.com>
2016-10-03 15:24:07 +0200

    added repositories and entities actor company and person

---
+++ src/AppBundle/Entity/Actor.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Actor
+ *
+ * @InheritanceType("JOINED")
+ * @DiscriminatorColumn(name="discr", type="string")
+ * @DiscriminatorMap({"actor" = "Actor", "person" = "Person"}, {"actor" = "Actor", "company" = "Company"})
+ *
+ * @ORM\Table(name="actor")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ActorRepository")
+ */
+class Actor
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="tlf", type="string", length=22, nullable=true)
+     */
+    private $tlf;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="images", type="array")
+     */
+    private $images;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="key_knowledges", type="array")
+     */
+    private $keyKnowledges;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="field", type="string", length=255, nullable=true)
+     */
+    private $field;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=45)
+     */
+    private $email;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set tlf
+     *
+     * @param string $tlf
+     *
+     * @return Actor
+     */
+    public function setTlf($tlf)
+    {
+        $this->tlf = $tlf;
+
+        return $this;
+    }
+
+    /**
+     * Get tlf
+     *
+     * @return string
+     */
+    public function getTlf()
+    {
+        return $this->tlf;
+    }
+
+    /**
+     * Set images
+     *
+     * @param array $images
+     *
+     * @return Actor
+     */
+    public function setImages($images)
+    {
+        $this->images = $images;
+
+        return $this;
+    }
+
+    /**
+     * Get images
+     *
+     * @return array
+     */
+    public function getImages()
+    {
+        return $this->images;
+    }
+
+    /**
+     * Set keyKnowledges
+     *
+     * @param array $keyKnowledges
+     *
+     * @return Actor
+     */
+    public function setKeyKnowledges($keyKnowledges)
+    {
+        $this->keyKnowledges = $keyKnowledges;
+
+        return $this;
+    }
+
+    /**
+     * Get keyKnowledges
+     *
+     * @return array
+     */
+    public function getKeyKnowledges()
+    {
+        return $this->keyKnowledges;
+    }
+
+    /**
+     * Set field
+     *
+     * @param string $field
+     *
+     * @return Actor
+     */
+    public function setField($field)
+    {
+        $this->field = $field;
+
+        return $this;
+    }
+
+    /**
+     * Get field
+     *
+     * @return string
+     */
+    public function getField()
+    {
+        return $this->field;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Person
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
+
---
+++ src/AppBundle/Entity/Person.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Person
+ *
+ * @ORM\Table(name="person")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PersonRepository")
+ */
+class Person
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=100)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=100)
+     */
+    private $lastName;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return Person
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return Person
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+}
+
