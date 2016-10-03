dfb1ee445d534f1d086aa3dac8ee2d66443831b5
Jonas Laksen <jonaslaksen@live.com>
2016-10-03 15:24:07 +0200

    added repositories and entities actor company and person

---
+++ src/AppBundle/Entity/Company.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Company
+ *
+ * @ORM\Table(name="company")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CompanyRepository")
+ */
+class Company
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=100)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="org_nr", type="string", length=18, unique=true)
+     */
+    private $orgNr;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Company
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set orgNr
+     *
+     * @param string $orgNr
+     *
+     * @return Company
+     */
+    public function setOrgNr($orgNr)
+    {
+        $this->orgNr = $orgNr;
+
+        return $this;
+    }
+
+    /**
+     * Get orgNr
+     *
+     * @return string
+     */
+    public function getOrgNr()
+    {
+        return $this->orgNr;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Company
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
