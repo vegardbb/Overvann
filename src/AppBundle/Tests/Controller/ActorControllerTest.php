45fedf8efe0f0edb422d204c46684cc77a7da661
Jonas Laksen <jonaslaksen@live.com>
2016-10-06 14:47:38 +0200

    create test for person and company creation

--- src/AppBundle/Tests/Controller/ActorControllerTest.php
+++ src/AppBundle/Tests/Controller/ActorControllerTest.php
@@ -24,11 +24,42 @@ class ActorControllerTest extends WebTestCase
             ->link();
         $crawler = $client->click($link);
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $form = $crawler->selectButton('save')->form();
-        $form['email'] = "{$this->generateRandomString(10)}@{$this->generateRandomString(10)}.com";
-        $form['name'] = $this->generateRandomString(10);
-        $form['type'] = $this->generateRandomString(10);
-        $form['org_nr'] = $this->generateRandomString(10);
+
+        $form = $crawler->selectButton('company[save]')->form();
+        $name = $this->generateRandomString(10);
+        $form['company[name]'] = $name;
+        $form['company[email]'] = "{$this->generateRandomString(10)}@{$this->generateRandomString(10)}.com";
+        $form['company[type]'] = $this->generateRandomString(10);
+        $form['company[org_nr]'] = $this->generateRandomString(10);
         $crawler = $client->submit($form);
+
+        $crawler = $client->followRedirect();
+        $this->assertContains($name, $client->getResponse()->getContent());
+    }
+
+
+    public function testCreatePerson()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/actor');
+        $links = $crawler
+            ->filter('a:contains("Lag person")');
+        $this->assertEquals(1, $links->count());
+
+        $link = $links
+            ->eq(0)
+            ->link();
+        $crawler = $client->click($link);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $form = $crawler->selectButton('person[save]')->form();
+        $name = $this->generateRandomString(10);
+        $form['person[firstName]'] = $name;
+        $form['person[lastName]'] = $this->generateRandomString(20);
+        $form['person[email]'] = "{$this->generateRandomString(10)}@{$this->generateRandomString(10)}.com";
+        $crawler = $client->submit($form);
+
+        $crawler = $client->followRedirect();
+        $this->assertContains($name, $client->getResponse()->getContent());
     }
 }
