cbe0bd904619d865f44fee1d1e91b11060ead9d2
Pål Bergerskogen <pal.bergerskogen@gmail.com>
2016-10-19 11:08:42 +0200

    Fix typo

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -18,8 +18,8 @@ class UserController extends Controller
 		// 1) build the form
 		$user = new User();
 		$form = $this->createForm(UserType::class, $user, array(
-        'environment' => $this->->get( 'kernel' )->getEnvironment()
-    ));
+        'environment' => $this->get( 'kernel' )->getEnvironment()
+    	));
 
 		// 2) handle the submit (will only happen on POST)
 		$form->handleRequest($request);
