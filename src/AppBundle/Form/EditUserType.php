d0721aa8e434e38ae9caadfe7a3448024cd05eaa
Vegard B. Bugge <vegardbb@stud.ntnu.no>
2016-10-25 16:01:16 +0200

    Define new form type

---
+++ src/AppBundle/Form/EditUserType.php
@@ -0,0 +1,33 @@
+<?php
+
+// src/AppBundle/Form/UserType.php
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class EditUserType extends AbstractType
+{
+	// Seperate form for editing user information. ONLY to be used by editors.
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('email', EmailType::class)
+			->add('lastName', TextType::class)
+			->add('firstName', TextType::class)
+			->add('phone', TextType::class)
+
+			->add('save', SubmitType::class, array('label' => 'Registrer bruker'));
+	}
+
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults(array(
+			'data_class' => 'AppBundle\Entity\User',
+		));
+	}
+}
