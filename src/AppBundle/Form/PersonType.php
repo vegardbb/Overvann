f635589ae09516f58ca6dd5bfbc20b76833b61cf
Vegard B. Bugge <vegardbb@stud.ntnu.no>
2016-10-30 16:18:13 +0100

    Use FileType in PersonType form

--- src/AppBundle/Form/PersonType.php
+++ src/AppBundle/Form/PersonType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Gregwar\CaptchaBundle\Type\CaptchaType;
