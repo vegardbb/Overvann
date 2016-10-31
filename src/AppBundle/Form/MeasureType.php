357a81f8bd8f26b9e5ea4733fa221cd06247c49b
Thomas Olsen <thomol@stud.ntnu.no>
2016-10-31 13:02:16 +0100

    Added help buttons for measures (needs more descriptive messages), added person selection in create company.

--- src/AppBundle/Form/MeasureType.php
+++ src/AppBundle/Form/MeasureType.php
@@ -23,17 +23,17 @@ class MeasureType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class, array('label' => 'Tittel', 'attr' => array('placeholder' => 'Tittel på tiltak')))
-            ->add('totalArea', NumberType::class, array('attr' => array('placeholder' => 'Totalt areal')))
-            ->add('costs', NumberType::class, array('attr' => array('placeholder' => 'Totale kostnader')))
-            ->add('technicalFunctions', TextareaType::class, array('attr' => array('placeholder' => 'Tekniske funksjoner')))
-            ->add('elaboration', TextareaType::class, array('attr' => array('placeholder' => 'Utdypning')))
-            ->add('dimentionalDemands', TextareaType::class, array('label' => 'Dimensjonerende krav','attr' => array('placeholder' => 'Oppsummering'),'label_attr' => array('id' => 'description_label')))
-            ->add('additionalValues', TextareaType::class, array('attr' => array('placeholder' => 'Nytteverdier')))
-            ->add('geometricDesignElaboration', TextareaType::class, array('attr' => array('placeholder' => 'Utdypning av geometrisk utforming')))
-            ->add('constructionDetails', TextareaType::class, array('attr' => array('placeholder' => 'Utdypning av geometrisk utforming')))
-            ->add('maintenance', TextareaType::class, array('attr' => array('placeholder' => 'Vedlikehold')))
-            ->add('experiencesGained', TextareaType::class, array('attr' => array('placeholder' => 'Erfaringer og tips')))
+            ->add('title', TextType::class, array('label' => 'Tittel', 'attr' => array('placeholder' => 'Tittel på tiltak','help' => 'My help msg')))
+            ->add('totalArea', NumberType::class, array('label' => 'Totalt areal','attr' => array('placeholder' => 'Totalt areal','help' => 'My help msg')))
+            ->add('costs', NumberType::class, array('label' => 'Kostnad','attr' => array('placeholder' => 'Totale kostnader','help' => 'My help msg')))
+            ->add('technicalFunctions', TextareaType::class, array('label' => 'Tekniske funksjoner','attr' => array('placeholder' => 'Tekniske funksjoner','help' => 'My help msg')))
+            ->add('elaboration', TextareaType::class, array('label' => 'Utdypning for tekniske funksjoner','attr' => array('placeholder' => 'Utdypning','help' => 'My help msg')))
+            ->add('dimentionalDemands', TextareaType::class, array('label' => 'Dimensjonerende krav','attr' => array('placeholder' => 'Oppsummering','help' => 'My help msg'),'label_attr' => array('id' => 'description_label')))
+            ->add('additionalValues', TextareaType::class, array('label' => 'Tilleggsverdier','attr' => array('placeholder' => 'Nytteverdier','help' => 'My help msg')))
+            ->add('geometricDesignElaboration', TextareaType::class, array('label' => 'Geometrisk utforming','attr' => array('placeholder' => 'Utdypning av geometrisk utforming','help' => 'My help msg')))
+            ->add('constructionDetails', TextareaType::class, array('label' => 'Konstruksjonsdetaljer og anleggelse','attr' => array('placeholder' => 'Utdypning av geometrisk utforming','help' => 'My help msg')))
+            ->add('maintenance', TextareaType::class, array('label' => 'Drift, vedlikehold og oppfølging','attr' => array('placeholder' => 'Vedlikehold','help' => 'My help msg')))
+            ->add('experiencesGained', TextareaType::class, array('label' => 'Erfaringer og tips','attr' => array('placeholder' => 'Erfaringer og tips','help' => 'My help msg')))
             ->add('save', SubmitType::class, array('label' => 'Legg til'));
     }
 
